ad765c220d2d8f27c29161ad10919a191fba7bac
haldamir95 <haldamir.95@gmail.com>
2020-04-18 17:36:53 -0600

    Presentacion

--- hello_world.py
+++ hello_world.py
@@ -19,7 +19,7 @@ def wrap_html(message):
 
 @app.route('/')
 def hello_world():
-    message = 'SA Tarea7 201314733'
+    message = 'SA Tarea 7 201314733'
     html = wrap_html(message)
     return html
 
